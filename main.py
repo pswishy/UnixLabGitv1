373f4a4a8513be91667ae55afde84524c098f0e0
Pierce Medy <piercemedy24@gmail.com>
2022-03-14 18:45:18 -0400

    block format

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import sys
 num = sys.argv
 key = int(num[1])
 
-empty_list = []
+all_letters = []
+
+blocks = []
+seperated = []
 for line in sys.stdin:
     phrase = line
     break
@@ -12,7 +15,22 @@ for chars in phrase:
     if ord(chars) >= 65 and ord(chars) <= 90: #characters that need to be encrypted 
         #print(ord(chars))
         if ord(chars) + key <= 90: # character in valid range
-            print(chr(ord(chars) + key ))
+            #print(chr(ord(chars) + key ))
+            all_letters.append(chr(ord(chars) + key ))
 
         else:
-            print(chr(ord(chars) - 26 + key))
+            #print(chr(ord(chars) - 26 + key)) # change value
+            all_letters.append(chr(ord(chars) + key ))
+
+for letters in all_letters:
+
+    if len(blocks) <= 5:
+        blocks.append(letters)
+    
+    else:
+        seperated.append(blocks)
+        blocks = []
+
+print(seperated)
+
+
