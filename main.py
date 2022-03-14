09815a93598e1f5fa31e0200eaee421cbd63c175
Pierce Medy <piercemedy24@gmail.com>
2022-03-14 19:34:06 -0400

    # logic for correct block output

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ num = sys.argv
 key = int(num[1])
 
 all_letters = []
-
+answer_string = ""
 blocks = []
+ten_per_line = []
 seperated = []
 for line in sys.stdin:
     phrase = line
@@ -22,15 +23,40 @@ for chars in phrase:
             #print(chr(ord(chars) - 26 + key)) # change value
             all_letters.append(chr(ord(chars) + key ))
 
+#print(all_letters)
 for letters in all_letters:
-
-    if len(blocks) <= 5:
+    if len(blocks) <= 4:
         blocks.append(letters)
-    
+
     else:
         seperated.append(blocks)
         blocks = []
+        blocks.append(letters)
+    
+    
+        
+        
 
 print(seperated)
+stdout_fileno = sys.stdout
 
+# logic for correct block output
+for blocks in seperated:
+    blocks = "".join(blocks)
+    #ten_per_line.append(blocks)
+
+    #rint(blocks, len(ten_per_line), ten_per_line,answer_string)
+    if len(ten_per_line) < 10:
+        ten_per_line.append(blocks)
+        answer_string += blocks + " "
+    else:
+        answer_string += "\n" + blocks + " "
+        
+        ten_per_line = []
+        ten_per_line.append(blocks)
+
+
+print(answer_string)
+    
+    #stdout_fileno.write(blocks + " ")
 
