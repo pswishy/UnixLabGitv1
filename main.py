af0cde0c3ca2a44992572f547fea751ee9401b06
Pierce Medy <piercemedy24@gmail.com>
2022-03-14 18:23:29 -0400

    cipher logic

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import sys
 num = sys.argv
-key = num[1]
-print(num, key)
+key = int(num[1])
 
+empty_list = []
 for line in sys.stdin:
-    print(line)
+    phrase = line
     break
-print('done')
+
+for chars in phrase:
+    chars = chars.upper()
+    if ord(chars) >= 65 and ord(chars) <= 90: #characters that need to be encrypted 
+        #print(ord(chars))
+        if ord(chars) + key <= 90: # character in valid range
+            print(chr(ord(chars) + key ))
+
+        else:
+            print(chr(ord(chars) - 26 + key))
